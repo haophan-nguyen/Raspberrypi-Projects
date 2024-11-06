6a93f349109c520294eae792ce59bf28a5e59b29
haophan-nguyen <hao.nguyen.phan.2402@gmail.com>
2024-11-06 15:20:22 +0000

    00: remove binary file

--- my_test/module/00/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/mypi/workspace/Raspberrypi-Projects/my_test/module/00/Module.symvers :=  scripts/mod/modpost -M -m -a      -o /home/mypi/workspace/Raspberrypi-Projects/my_test/module/00/Module.symvers -T /home/mypi/workspace/Raspberrypi-Projects/my_test/module/00/modules.order -i Module.symvers -e 
--- my_test/module/00/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/mypi/workspace/Raspberrypi-Projects/my_test/module/00/modules.order := {   echo /home/mypi/workspace/Raspberrypi-Projects/my_test/module/00/00_hello_kernel.o; :; } > /home/mypi/workspace/Raspberrypi-Projects/my_test/module/00/modules.order
